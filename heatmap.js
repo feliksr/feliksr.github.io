2c7e3b92bd18786d482a94814bacc4a3c77f2f83
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-22 17:04:54 -0600

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -23,7 +23,7 @@ class Heatmap {
 
     async fetchData() {
         try {
-            const response = await fetch(`https://froyzen.pythonanywhere.com/Target/${this.channel}`);
+            const response = await fetch(`https://froyzen.pythonanywhere.com/`);
             const responseData = await response.json();
             return responseData;
 
