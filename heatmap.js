29eb293862e9607941ad77c91d8b5ca089feaaaf
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-23 22:00:25 -0600

    update

--- heatmap.js
+++ heatmap.js
@@ -10,7 +10,7 @@ class Heatmap {
         };
         this.container = config.container || "#heatmapContainer";
         this.channel = config.channel || 1;
-        document.getElementById('channelDisplay').innerText = `Channel: ${this.channel}`;
+        document.getElementById('channelDisplay').textContent = `Channel: ${this.channel}`;
         document.getElementById("pageTitle").textContent = `Channel: ${this.channel}`;
 
         this.currentTrial = 1;
