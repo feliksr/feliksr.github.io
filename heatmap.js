6a7deecf257a4f3b0fffa0eeb73dc2b5e9806c80
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-25 06:09:00 -0600

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -100,7 +100,7 @@ class Heatmap {
             .attr("x", d => this.xScale(d.time))
             .attr("y", d => {
                 console.log(d.frequency);
-                return this.yScale(d.frequency);
+                return this.yScale(d.time);
             })
             .attr("width", this.width / this.timeWavelet.length)
             .attr("height", this.height / this.scale.length)
