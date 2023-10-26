f8fd872fa44d09fcc6d30f665ff2124fa750b1bf
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-25 21:33:14 -0600

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -91,7 +91,7 @@ class Heatmap {
             
         this.svg.append("g")
             .attr("class", "x-axis")
-            .attr("transform", `translate(0,${this.height - this.margin.top})`)
+            .attr("transform", `translate(0,${this.height - this.margin.top - 10})`)
             .call(d3.axisBottom(this.xScale).ticks(5));
 
         this.svg.selectAll("rect")
