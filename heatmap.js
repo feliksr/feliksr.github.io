53dbb43b8c4a718dc036143cfc8ec923aca899ef
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-29 16:21:51 -0600

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -107,7 +107,7 @@ class Heatmap {
                     .append("text")
                     .attr("class", "x-axis-label")
                     .attr("x", this.width / 2)  
-                    .attr("y", heightSVG + this.margin.bottom/2) 
+                    .attr("y", heightSVG) 
                     .style("text-anchor", "middle")
                     .text("Time from Response (sec)")
             } 
