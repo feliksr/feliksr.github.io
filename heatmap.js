2c38d5fefd39dd0ffae4ea6746477af220fe88e6
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-29 09:41:30 -0600

    update

--- heatmap.js
+++ heatmap.js
@@ -67,56 +67,59 @@ class Heatmap {
                 .select("svg")
                 .remove(); 
 
-        const bin = frequencyBins[index];
-        const filteredData = this.singleTrialData.filter(d => d.frequency >= bin.min && d.frequency <= bin.max);
-        
-        const allFreqBins = new Set(this.singleTrialData.map(d => d.frequency)).size
-        const numFreqBins = new Set(filteredData.map(d => d.frequency)).size
-        const numTimeBins = new Set(filteredData.map(d => d.time)).size
-
-        const svg = d3.select(container).append("svg")
-            .attr("width", this.width + this.margin.left + this.margin.bottom)
-            .attr("height", this.height * (numFreqBins/allFreqBins) + this.margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${this.margin.left}, 0)`);
-        
-        this.xScale = d3.scaleLinear()
-            .range([0, this.width])
-            .domain([d3.min(this.singleTrialData, d => d.time), d3.max(this.singleTrialData, d => d.time)]);
-        
-        this.yScale = d3.scaleLog()
-            .range([0, this.height * (numFreqBins/allFreqBins)])
-            .domain([d3.max(filteredData, d => d.frequency),d3.min(filteredData, d => d.frequency)]);
-
-        svg.append("g")
-            .attr("class", "y-axis")
-            .call(d3.axisLeft(this.yScale)
-            .tickFormat(d => {return parseFloat(d.toPrecision(2))}));
+            const bin = frequencyBins[index];
+            const filteredData = this.singleTrialData.filter(d => d.frequency >= bin.min && d.frequency <= bin.max);
             
-        svg.append("g")
-            .attr("class", "x-axis")
-            .call(d3.axisBottom(this.xScale).ticks(5))
-            .attr("transform", `translate(0, ${this.height * (numFreqBins/allFreqBins)})`);
-        
-        svg.select(".x-axis")
-            .append("text")
-            .attr("class", "x-axis-label")
-            .attr("x", this.width / 2)
-            .attr("y", 40) 
-            .style("text-anchor", "middle")
-            .text("Time from Response (sec)");
-               
-        svg.selectAll("rect")
-            .data(filteredData)
-            .enter()
-            .append("rect")
-            .attr("x", d => this.xScale(d.time))
-            .attr("y", d => this.yScale(d.frequency) -  (this.height * (numFreqBins/allFreqBins))/(numFreqBins -1))
-            .attr("width", this.width /  (numTimeBins - 1))
-            .attr("height", this.height * (numFreqBins/allFreqBins) / (numFreqBins - 1))
-            .attr("shape-rendering", "crispEdges")
+            const allFreqBins = new Set(this.singleTrialData.map(d => d.frequency)).size
+            const numFreqBins = new Set(filteredData.map(d => d.frequency)).size
+            const numTimeBins = new Set(filteredData.map(d => d.time)).size
+
+            const svg = d3.select(container).append("svg")
+                .attr("width", this.width + this.margin.left + this.margin.bottom)
+                .attr("height", this.height * (numFreqBins/allFreqBins) + this.margin.bottom)
+                .append("g")
+                .attr("transform", `translate(${this.margin.left}, 0)`);
+            
+            this.xScale = d3.scaleLinear()
+                .range([0, this.width])
+                .domain([d3.min(this.singleTrialData, d => d.time), d3.max(this.singleTrialData, d => d.time)]);
+            
+            this.yScale = d3.scaleLog()
+                .range([0, this.height * (numFreqBins/allFreqBins)])
+                .domain([d3.max(filteredData, d => d.frequency),d3.min(filteredData, d => d.frequency)]);
+
+            svg.append("g")
+                .attr("class", "y-axis")
+                .call(d3.axisLeft(this.yScale)
+                .tickFormat(d => {return parseFloat(d.toPrecision(2))}));
+
+            if (this.container.id === "container3") {    
+                svg.append("g")
+                    .attr("class", "x-axis")
+                    .call(d3.axisBottom(this.xScale).ticks(5))
+                    .attr("transform", `translate(0, ${this.height * (numFreqBins/allFreqBins)})`);
+            
+            
+                svg.select(".x-axis")
+                    .append("text")
+                    .attr("class", "x-axis-label")
+                    .attr("x", this.width / 2)
+                    .attr("y", 40) 
+                    .style("text-anchor", "middle")
+                    .text("Time from Response (sec)");
+            }        
+            svg.selectAll("rect")
+                .data(filteredData)
+                .enter()
+                .append("rect")
+                .attr("x", d => this.xScale(d.time))
+                .attr("y", d => this.yScale(d.frequency) -  (this.height * (numFreqBins/allFreqBins))/(numFreqBins -1))
+                .attr("width", this.width /  (numTimeBins - 1))
+                .attr("height", this.height * (numFreqBins/allFreqBins) / (numFreqBins - 1))
+                .attr("shape-rendering", "crispEdges")
         })
     }
+
     drawHeatmap() {
         this.containers.forEach((container, index) => {
             const bin = frequencyBins[index];
