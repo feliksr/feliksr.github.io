657fd0f2e1fa23c2435aeb81cc38ad0b14987f3e
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-31 22:45:17 -0600

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -96,7 +96,7 @@ class Colorbar {
                 .domain([0, maxColor]) 
                 .range([heatmap.svgHeights[index], 0]);  
 
-        heatmap.select(container).select(svg).append('g')
+        heatmap.getSelection(container).select("svg").append('g')
             .attr("class", "colorbar-axis")
             .call(d3.axisRight(colorbarScale).ticks(5))
             .attr("transform", `translate(${this.width}, 0)`); 
