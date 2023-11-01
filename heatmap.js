7d4dab1dd25aa0e29674d344179b43f9b0711273
FeliksR <100524288+FeliksR@users.noreply.github.com>
2023-10-31 23:04:11 -0600

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -80,7 +80,7 @@ class Colorbar {
             .enter().append("rect")
             .attr("class", "colorbar-rect")
             .attr("x", 0)
-            .attr("y", (_, i) => (1 - i) * rectHeight)
+            .attr("y", (_, i) => (this.numStops - i) * rectHeight)-rectHeight
             .attr("width", this.width)
             .attr("height", rectHeight)
             .attr("fill", d => d3.interpolateViridis(d / (this.numStops)))
